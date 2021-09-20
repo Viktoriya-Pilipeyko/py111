df0559be14bedbc6d0a53b508324ff26fa1f71c5
Viktoriya-Pilipeyko <vika.252@mail.ru>
2021-09-20 13:13:53 +0300

    словари - задания

---
+++ dict.py
@@ -0,0 +1,10 @@
+#будет выводить значение по заданному ключу
+dict = {'Hello' : '', 'Bye' : '', 'List' : ''}
+your_input = input('введите для hello: ')
+dict['Hello'] = your_input
+your_input = input('введите для bye: ')
+dict['Bye'] = your_input
+your_input = input('введите для List: ')
+dict['List'] = your_input
+print(dict)
+
