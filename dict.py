ef855ffe991159fc7c43276a21a58770669fa002
Viktoriya-Pilipeyko <vika.252@mail.ru>
2021-09-20 14:33:17 +0300

    добавлено решение заданий 2 и 3

--- dict.py
+++ dict.py
@@ -1,10 +1,23 @@
 #будет выводить значение по заданному ключу
-dict = {'Hello' : '', 'Bye' : '', 'List' : ''}
+""" dict = {'Hello' : '', 'Bye' : '', 'List' : ''}
 your_input = input('введите для hello: ')
 dict['Hello'] = your_input
 your_input = input('введите для bye: ')
 dict['Bye'] = your_input
 your_input = input('введите для List: ')
 dict['List'] = your_input
-print(dict)
+print(dict) """
 
+#Программа должна производить поиск по значению и выдавать ключ
+""" dict1 = {'Hello' : 'Hi', 'Bye' : 'Goodbye', 'List' : 'Array'}
+inputvalue = input('Введите значение: ')
+for key, value in dict1.items():
+    if value == inputvalue:
+        print(key)
+    else:
+        continue
+ """
+ #принимает список строк и выводит количество повторений данных строк в списке
+list = ['abc', 'bcd', 'abc', 'abd', 'abd', 'dcd', 'abc']
+dictcount = {i:list.count(i) for i in list}
+print (dictcount)
