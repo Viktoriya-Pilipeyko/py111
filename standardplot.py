eedb01c5f2788630ce6bb725f8490a439edea90a
Viktoriya-Pilipeyko <vika.252@mail.ru>
2021-09-21 13:47:01 +0300

    задание по теме модули

--- standardplot.py
+++ standardplot.py
@@ -1,34 +1,32 @@
 
-str = input('Введите: ')
+def passwordrecom(str):
+    password = str
+    recomend = 'Пароль то, что нужно!'
+    if len(password) < 12:
+        recomend = 'количество символов в пароле должно быть 12 и более'
+    if password.isalnum():
+        recomend += ', пароль должен включать и цифры и буквы'
+    b = 0
+    a = 0
+    for l in password:
+        upp = l.isupper()
+        if upp:
+            continue
+        else:
+            b+=1
+    for l in password:
+        dow = l.islower()
+        if dow:
+            continue
+        else:
+            a +=1
+    if len(password) == b:
+        recomend += ', в пароле должны быть заглавные буквы'
+    if  len(password) == a:
+        recomend += ', в пароле должны быть строчные буквы'
+    #if password.find('?') == -1:
+     #   recomend += ', необходим спецсимвол'
 
-print(str.title())
+    return(recomend)
 
-password = input('Введите пароль: ')
-
-recomend = 'Пароль то, что нужно!'
-if len(password) < 12:
-    recomend = 'количество символов в пароле должно быть 12 и более'
-if password.isalnum():
-    recomend += ', пароль должен включать и цифры и буквы'
-b = 0
-a = 0
-for l in password:
-    upp = l.isupper()
-    if upp:
-        continue
-    else:
-        b+=1
-for l in password:
-    dow = l.islower()
-    if dow:
-        continue
-    else:
-        a +=1
-if len(password) == b:
-    recomend += ', в пароле должны быть заглавные буквы'
-if  len(password) == a:
-    recomend += ', в пароле должны быть строчные буквы'
-if password.find('?') == -1:
-        recomend += ', необходим спецсимвол'
-
-print(recomend)
+#print(passwordrecom('88Qm#6Je&4Ji('))
