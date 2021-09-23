6f214b991af630bc0948cc370a11df194ea6b3f9
Viktoriya-Pilipeyko <vika.252@mail.ru>
2021-09-23 17:13:54 +0300

    factorial

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-from randome import randompass
+n = int(input())
 
-print(randompass)
+memlist = {}
+
+def facttt(f):
+    factorial = 1
+    try:
+        if memlist[f]:
+            print('for key ' + str(f) + 'value ' + str(memlist[f]))
+    except:  
+        for i in range(2, f+1):
+            factorial *= i
+            memlist[f] = factorial
+        print(str(factorial)+ ' factorial')
+print(facttt(n))
+print(facttt(n))
+print(facttt(n))
+print(memlist[n])
